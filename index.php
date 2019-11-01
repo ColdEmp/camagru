760df5ecee191d2c9e44117055dcf0c826309980
hbarnardWTC <wingarkorin@gmail.com>
2019-11-01 14:40:18 +0200

    I broke the header

--- index.php
+++ index.php
@@ -6,24 +6,45 @@
 	<Head>
 	<title>Camagru</title>
 	<link rel = "stylesheet" type = "text/css" href = "style/index.css"/>
-	<link rel = "stylesheet" type = "text/css" href = "style/w3.css"/>
 	</head>
 	<body>
-		<div>
-			<div id = "header">
+			<nav id = "side-menu" class = "side-nav">
+				<a href='#' class = btn-close onClick  = "closeSlideMenu()">&times;</a>  
+				<a href='#'>Home</a>
+				<a href='#'>Gallery</a>
+				<a href='#'>Profile</a>
+				<a href='#'>Logout</a>
+			</nav>
+			<div class = "header">
+			<span class = open-slide>
+					<a href= "#" onClick = "openSlideMenu()">
+						<svg width="30" height = "30">
+							<path d="M0,5 30,5" stroke = '#fff'
+							stroke-width = "5"/>
+							<path d="M0,14 30,14" stroke = '#fff'
+							stroke-width = "5"/>
+							<path d="M0,23 30,23" stroke = '#fff'
+							stroke-width = "5"/>
+						</svg>
+					</a>
+			</span>
 				<h1 class = "header title">Camagru Name</h1>
 			</div>
-			<div id = "container">
-				<div class="w3-sidebar w3-bar-block" style="width:25%">
-							<a href="#" class="w3-bar-item w3-button">Link 1</a>
-							<a href="#" class="w3-bar-item w3-button">Link 2</a>
-							<a href="#" class="w3-bar-item w3-button">Link 3</a>
-				</div>
-				<div id = "login">
-					<div class="ltitle">LOGIN</div>
-				</div>
+			<div id = "login">
+				<h4 class="ltitle">LOGIN</h4>
 			</div>
-		</div>
+			<script>
+			function openSlideMenu(){
+				document.getElementById('side-menu').style.width = '250px';
+
+				document.getElementById('side-menu').style.marginLeft = '250px';
+			}
+			function closeSlideMenu(){
+				document.getElementById('side-menu').style.width = '0px';
+
+				document.getElementById('side-menu').style.marginLeft = '0px';
+			}
+			</script>
 	</body>
 
 </HTML>
