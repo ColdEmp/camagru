1658497dbdc9671485e5e1f46dd7cfc7aa6cdb8c
ColdEmp <cdglanville@gmail.com>
2019-11-06 14:02:07 +0200

    updated pdo on setup.php

--- config/setup.php
+++ config/setup.php
@@ -5,25 +5,36 @@ $user = 'server01';
 $password = 'Password@php1';
 $dbname = 'camgru_db';
 
-// Set DSN
-//$dsn = 'mysql:host=' . $host . ';dbname=' . $bdname;
-
-// Create a PDO instance
-// $pdo = new PDO($dsn, $user, $password);//connection
-
-$connection = new mysqli($host,$user,$password);
-mysqli_query($connection,"CREATE DATABASE IF NOT EXISTS camagru_db");
-//$db = mysqli_connect($host,"server01","Password@php1","camagru_db");//unnessasary I think, see *
-mysqli_select_db($connection, 'camagru_db');//*
+try{
+$dsn = 'mysql:host=' . $host;
+// $dsn = 'mysql:host=' . $host . ';dbname=' . $bdname;
+$connection = new PDO($dsn, $user, $password);
+}catch(PDOException $e){
+    die('Connection Error: ' . $e->getMessage() );
+}
+//$connection = new mysqli($host,$user,$password);
+$statement = $connection->prepare("CREATE DATABASE IF NOT EXISTS $dbname");
+if($statement->execute())
+{
+    echo "Success Database";
+}
+//mysqli_query($connection,"CREATE DATABASE IF NOT EXISTS camagru_db");
+//$db = mysqli_connect($host,"server01","Password@php1","camagru_db");
+// mysqli_select_db($connection, 'camagru_db');
 
 //user table
 $username = "username TEXT(50)";
 $firstname = "firstname TEXT(50)";
 $surname = "surname TEXT(50)";
 $email = "email TEXT(50)";
-$password = "password TEXT(50000)";
+$user_password = "user_password TEXT(50000)";
 $notification_pref = "notifications BOOLEAN";
-mysqli_query($connection,"CREATE TABLE IF NOT EXISTS Users ($username,$firstname,$surname,$password,$email,$notification_pref)");
+$statement = $connection->prepare("CREATE TABLE IF NOT EXISTS Users ($username,$firstname,$surname,$user_password,$email,$notification_pref)");
+if($statement->execute())
+{
+    echo "Success Users";
+}
+// mysqli_query($connection,"CREATE TABLE IF NOT EXISTS Users ($username,$firstname,$surname,$password,$email,$notification_pref)");
 
 //image table
 $image = "image TEXT(100)";
@@ -31,6 +42,11 @@ $imagename = "imagename TEXT(50)";
 $uploader = "uploader TEXT(50)";
 $date_created = "date_created DATE";
 $edited = "edited BOOLEAN";
-mysqli_query($connection,"CREATE TABLE IF NOT EXISTS Images ($image,$imagename,$uploader,$date_created,$edited)");
+$statement = $connection->prepare("CREATE TABLE IF NOT EXISTS Images ($image,$imagename,$uploader,$date_created,$edited)");
+if($statement->execute())
+{
+    echo "Success Images";
+}
+// mysqli_query($connection,"CREATE TABLE IF NOT EXISTS Images ($image,$imagename,$uploader,$date_created,$edited)");
 
 ?>
