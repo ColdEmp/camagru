7f81d9dde9d54b310ea21774297fa575d713a157
hbarnardWTC <wingarkorin@gmail.com>
2019-11-10 11:34:08 +0200

    Bulma

--- index.php
+++ index.php
@@ -1,19 +1,34 @@
-<HTML lang="en">
+<!-- <HTML lang="en">
 	<Head>
 	<title>Camagru</title>
-<!--	<iframe style="display:none" src="https://www.youtuberepeater.com/watch?v=5xxQs34UMx4#gsc.tab=0" frameborder="0" allowfullscreen allow="autoplay" loop="true"></iframe> -->
-		<!-- <iframe style="display:none" src="https://www.youtuberepeater.com/watch?v=tt5SdjfNuGU#gsc.tab=0" frameborder="0" allowfullscreen allow="autoplay" loop="true"></iframe>  -->
-		<link rel = "stylesheet" href = "./style/camindex.css"/>
-	</head>
-	<body>
-			<!-- <nav id = "side-menu" class = "side-nav">
-				<a href='#' class = btn-close onClick  = "closeSlideMenu()">&times;</a>  
-				<a href='#'>Home</a>
-				<a href='#'>Gallery</a>
-				<a href='#'>Profile</a>
-				<a href='#'>Logout</a>
-			</nav> -->
-			<div class = navbar>
+		<iframe style="display:none" src="https://www.youtuberepeater.com/watch?v=5xxQs34UMx4#gsc.tab=0" frameborder="0" allowfullscreen allow="autoplay" loop="true"></iframe>
+		<iframe style="display:none" src="https://www.youtuberepeater.com/watch?v=tt5SdjfNuGU#gsc.tab=0" frameborder="0" allowfullscreen allow="autoplay" loop="true"></iframe>
+		<link rel = "stylesheet" href = "./style/bulma.css"/>
+		<link rel = "stylesheet" href = "./style/index.css"/>
+	</head> -->
+	<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello Bulma!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+  </head>
+  <body>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+    </div>
+  </section>
+  </body>
+</html>
+			<!-- <div class = navbar>
 				<span class = open-slide>
 					<a href= "#" onClick = "openSlideMenu()">
 						<svg width="30" height = "30">
@@ -27,12 +42,6 @@
 					</a>
 					<h1 id = "title">Camagru Name</h1>
 				</span>
-				<!-- <ul class = "navbar-nav">
-				<li><a href='#'>Home</a></li>
-				<li><a href='#'>Gallery</a></li>
-				<li><a href='#'>Profile</a></li>
-				<li><a href='#'>Logout</a></li>
-				</ul> -->
 			</div>
 			<div id = "side-menu" class = "side-nav">
 				<a href='#' class = btn-close onClick  = "closeSlideMenu()">&times;</a>  
@@ -40,11 +49,8 @@
 				<a href='#'>Gallery</a>
 				<a href='#'>Profile</a>
 				<a href='#'>Logout</a>
-			</div>
-			<div id = "login">
-				<h4 class="ltitle">LOGIN</h4>
-			</div>
-			<script>
+			</div> -->
+			<!-- <script>
 				function openSlideMenu()
 				{
 					document.getElementById('side-menu').style.width = '150px';
@@ -53,6 +59,6 @@
 				{
 					document.getElementById('side-menu').style.width = '0px';
 				}
-			</script>
-	</body>
-</HTML>
+			</script> -->
+	<!-- </body>
+</HTML> -->
