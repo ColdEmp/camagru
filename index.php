bcaaa672b33d57a9caf72650fd095cbf91bdbee4
hbarnardWTC <wingarkorin@gmail.com>
2019-11-12 11:51:20 +0200

    reee

--- index.php
+++ index.php
@@ -11,54 +11,75 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hello Bulma!</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
-    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    <title>Camagru</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+	<link rel="stylesheet" href="./style/index.css">
   </head>
   <body>
-  <section class="section">
-    <div class="container">
-      <h1 class="title has-text-centered">
-        Hello World
-      </h1>
-      <p class="subtitle">
-        My first website with <strong>Bulma</strong>!
-      </p>
-    </div>
-  </section>
+	<!-- Hero Banner-->
+   	<div class="hero has-background-grey-dark has-text-centered">
+    	<h1 class="title is-1 has-text-light">CAMAGRU</h1>
+	 	<p class="subtitle has-text-light">A social media app!</p>
+	</div>
+	<section class="section is-centered">
+	<!-- Main body -->
+		<div class="columns">
+			<!--Side Menu-->
+			<div class="column">
+					<aside class="menu">
+
+						<p class="menu-label">
+							Profile
+						</p>
+						<ul class="menu-list">
+							<li><a class = "is-inactive">View Profile</a></li>
+							<li><a>Edit Profile</a></li>
+							<li><a>Logout</a></li>
+						</ul>
+						<p class="menu-label">
+							Editing
+						</p>
+						<ul class="menu-list">
+							<li><a>Edit a photo</a></li>
+							<li><a>View your gallery</a></li>
+						  	<li><a>View camagru gallery</a></li>
+						</ul>
+					</aside>
+			</div>
+			<!-- END OF SIDE MENU -->
+			<!-- LOGIN -->
+			<div class="column">
+				<div class="box has-text-centered has-background-grey-dark" id = "loginbox">
+					<h1 class="title is-3 has-text-light">LOGIN</h1>
+					<div class="field">
+							<div class="control">
+									<input class="input is-large" type="text" placeholder="Username">
+							</div>
+					</div>
+					<div class="field">
+						<div class="control">
+							<input class="input is-large" type="password" placeholder="Password">
+						</div>
+					</div>
+					<div>
+						<button class="button" id = "loginbutton">Login</button>
+						<a href="./signup.html" class="button" id = "signupbutton">Sign up</a>
+					</div>
+				</div>
+			</div>
+			<!-- END OF LOGIN -->
+			<div class="column"></div>
+		</div>
+		<!-- <div class="box has-background-grey">
+			<h1 class="title is-3">Login</h1>
+		</div> -->
+	</section>
+	<footer class="credits has-background-grey">
+		<div class="content has-text-centered has-text-light">
+			<p>Camagru by <a class ="has-text-light is-italic" href="https://github.com/cameronglanville">Cameron Glanville</a>,
+			 <a class ="has-text-light is-italic" href="https://github.com/hbarnardWTC">Heinrich Barnard</a>,
+			 <a class ="has-text-light is-italic" href="https://github.com/CaptainRedBear">Timothy Webb</a>.</p>
+		</div>
+	</footer>
   </body>
 </html>
-			<!-- <div class = navbar>
-				<span class = open-slide>
-					<a href= "#" onClick = "openSlideMenu()">
-						<svg width="30" height = "30">
-							<path d="M0,5 30,5" stroke = '#fff'
-							stroke-width = "5"/>
-							<path d="M0,14 30,14" stroke = '#fff'
-							stroke-width = "5"/>
-							<path d="M0,23 30,23" stroke = '#fff'
-							stroke-width = "5"/>
-						</svg>
-					</a>
-					<h1 id = "title">Camagru Name</h1>
-				</span>
-			</div>
-			<div id = "side-menu" class = "side-nav">
-				<a href='#' class = btn-close onClick  = "closeSlideMenu()">&times;</a>  
-				<a href='#'>Home</a>
-				<a href='#'>Gallery</a>
-				<a href='#'>Profile</a>
-				<a href='#'>Logout</a>
-			</div> -->
-			<!-- <script>
-				function openSlideMenu()
-				{
-					document.getElementById('side-menu').style.width = '150px';
-				}
-				function closeSlideMenu()
-				{
-					document.getElementById('side-menu').style.width = '0px';
-				}
-			</script> -->
-	<!-- </body>
-</HTML> -->
