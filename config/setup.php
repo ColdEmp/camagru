ae2c5d68a45acf45281c91eb088b9205299bb662
ColdEmp <cdglanville@gmail.com>
2019-11-15 09:23:58 +0200

    updated setup

--- config/setup.php
+++ config/setup.php
@@ -19,7 +19,7 @@ catch(PDOException $e)
 $statement = $connection->prepare("CREATE DATABASE IF NOT EXISTS $dbname");
 if($statement->execute())
 {
-    echo "Success Database\n";
+    echo "Success Database<br />";
 }
 //mysqli_query($connection,"CREATE DATABASE IF NOT EXISTS camagru_db");
 //$db = mysqli_connect($host,"server01","Password@php1","camagru_db");
@@ -28,29 +28,40 @@ if($statement->execute())
 $connection = open_connection();//new PDO($dsn, $user, $password);
 
 //user table
+$userId = "userId TEXT(50)";
 $username = "username TEXT(50)";
-$firstname = "firstname TEXT(50)";
-$surname = "surname TEXT(50)";
 $email = "email TEXT(50)";
-$user_password = "user_password TEXT(50000)";
-$notification_pref = "notifications BOOLEAN";
-$statement = $connection->prepare("CREATE TABLE IF NOT EXISTS Users ($username,$firstname,$surname,$email,$user_password,$notification_pref)");
+$password = "password TEXT(50000)";
+$varification_token = "varification_token TEXT(1000)";
+$varified = "varified BOOLEAN";
+$notifications = "notifications BOOLEAN";
+$statement = $connection->prepare("CREATE TABLE IF NOT EXISTS Users ($userId,$username,$email,$password,$varification_token,$varified,$notifications)");
 if($statement->execute())
 {
-    echo "Success Users\n";
+    echo "Success Users<br />";
 }
 // mysqli_query($connection,"CREATE TABLE IF NOT EXISTS Users ($username,$firstname,$surname,$password,$email,$notification_pref)");
 
 //image table
-$image = "image TEXT(100)";
-$imagename = "imagename TEXT(50)";
-$uploader = "uploader TEXT(50)";
-$date_created = "date_created DATE";
-$edited = "edited BOOLEAN";
-$statement = $connection->prepare("CREATE TABLE IF NOT EXISTS Images ($image,$imagename,$uploader,$date_created,$edited)");
+$imageId = "imageId TEXT(50)";
+$userId = "userId TEXT(50)";
+$image_src = "image_src TEXT(5000)";
+$name = "name TEXT(50)";
+$statement = $connection->prepare("CREATE TABLE IF NOT EXISTS Images ($imageId,$userId,$image_src,$name)");
 if($statement->execute())
 {
-    echo "Success Images\n";
+    echo "Success Images<br />";
 }
 // mysqli_query($connection,"CREATE TABLE IF NOT EXISTS Images ($image,$imagename,$uploader,$date_created,$edited)");
+
+//comment table
+$commentId = "commentId TEXT(50)";
+$imageId = "imageId TEXT(50)";
+$comment_text = "comment_text TEXT(5000)";
+$statement = $connection->prepare("CREATE TABLE IF NOT EXISTS Comments ($commentId,$imageId,$comment_text)");
+if($statement->execute())
+{
+    echo "Success Comments<br />";
+}
+
 ?>
