387b0f09d19348f399376168de6c9a2cec36dc04
hbarnardWTC <wingarkorin@gmail.com>
2019-11-15 14:03:27 +0200

    Pages

--- index.php
+++ index.php
@@ -1,77 +1,90 @@
-<?php
-	session_start();
-?>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
-	<link rel="stylesheet" href="./style/index.css">
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Camagru</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+	<link rel="stylesheet" href="../style/gallery.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
   <body>
 	<!-- Hero Banner-->
-   	<div class="hero has-background-grey-dark has-text-centered">
-    	<h1 class="title is-1 has-text-light">CAMAGRU</h1>
-	 	<p class="subtitle has-text-light">A social media app!</p>
+	<div class="Level has-background-grey-dark has-text-centered">
+		<div style = "display: inline;">
+			<a href = "../index.php" class = "button is-outlined hbutton">Home</a>
+			<h1 class="title is-1 has-text-light">CAMAGRU</h1>
+		</div>
+			 <p class="subtitle has-text-light">A social media app!</p>
 	</div>
-	<section class="section is-centered">
-	<!-- Main body -->
+	<!-- Main Content -->
+	<section class="section">
 		<div class="columns">
-			<!--Side Menu-->
-			<div class="column" style = "width : 0% !important">
-					<aside class="menu">
-						<p class="menu-label">
-							Profile
-						</p>
-						<ul class="menu-list">
-							<li><a href = "pages/viewProfile.php">View Profile</a></li>
-							<li><a>Logout</a></li>
-						</ul>
-						<p class="menu-label">
-							Editing
-						</p>
-						<ul class="menu-list">
-							<li><a>Editor</a></li>
-						  	<li><a href = "./pages/gallery.php">View gallery</a></li>
-						</ul>
-					</aside>
-			</div>
-			<!-- END OF SIDE MENU -->
-			<!-- LOGIN -->
 			<div class="column">
-				<div class="box has-text-centered has-background-grey-dark" id = "loginbox">
-					<h1 class="title is-3 has-text-light">LOGIN</h1>
-					<div class="field">
-						<div class="control">
-								<input class="input is-large" type="text" placeholder="Username">
-						</div>
-					</div>
-					<div class="field">
-						<div class="control">
-							<input class="input is-large" type="password" placeholder="Password">
-						</div>
-					</div>
-					<div>
-						<button class="button" id = "loginbutton">Login</button>
-						<a href="./pages/signup.php" class="button" id = "signupbutton">Sign up</a>
+				<aside class="menu">
+					<p class="menu-label">
+						Profile
+					</p>
+					<ul class="menu-list">
+						<li><a href = "./viewProfile.php">View Profile</a></li>
+						<li><a>Logout</a></li>
+					</ul>
+					<p class="menu-label">
+						Editing
+					</p>
+					<ul class="menu-list">
+						<li><a>Editor</a></li>
+						<li><a class = "inactivelink">View gallery</a></li>
+					</ul>
+				</aside>
+			</div>
+			<div class="column is-half">
+				<div class="box has-text-centered has-background-grey-dark" id = "prof_box">
+					<div class="block">
+						<img src="../images/illidan1.jpeg" alt="test" class="image">
+						<article class="media">
+							<figure class="media-left">
+								<p class="image is-64x64">
+								<img src="https://versions.bulma.io/0.8.0/images/placeholders/128x128.png">
+							</p>
+						</figure>
+						<div class="media-content">
+							<div class="content has-text-light">
+								<p>
+									<strong class="has-text-light is-4">USERNAME</strong>
+									<br>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+								</p>
+							</div>
+							<nav class="level is-mobile">
+								<div class="level-left">
+									<a class="level-item">
+										<span class="icon is-small"><i class="fa fa-comment"></i></span>
+									</a>
+								  	<a class="level-item">
+										<span class="icon is-small"><i class="fa fa-heart"></i></span>
+									</a>
+								</div>
+							  </nav>
+							</div>
+							<div class="media-right">
+							  <button class="delete"></button>
+							</div>
+						  </article>
 					</div>
 				</div>
 			</div>
-			<!-- END OF LOGIN -->
-			<div class="column"></div>
+			<div class="column">
+
+			</div>
 		</div>
-		<!-- <div class="box has-background-grey">
-			<h1 class="title is-3">Login</h1>
-		</div> -->
 	</section>
-	<footer class="credits has-background-grey">
-		<div class="content has-text-centered has-text-light">
-			<p>Camagru by <a class ="has-text-light is-italic" href="https://github.com/cameronglanville">Cameron Glanville</a>,
-			 <a class ="has-text-light is-italic" href="https://github.com/hbarnardWTC">Heinrich Barnard</a>,
-			 <a class ="has-text-light is-italic" href="https://github.com/CaptainRedBear">Timothy Webb</a>.</p>
-		</div>
-	</footer>
+		<footer class="credits has-background-grey">
+				<div class="content has-text-centered has-text-light">
+					<p>Camagru by <a class ="has-text-light is-italic" href="https://github.com/cameronglanville">Cameron Glanville</a>,
+					 <a class ="has-text-light is-italic" href="https://github.com/hbarnardWTC">Heinrich Barnard</a>,
+					 <a class ="has-text-light is-italic" href="https://github.com/CaptainRedBear">Timothy Webb</a>.</p>
+				</div>
+		</footer>
   </body>
 </html>
