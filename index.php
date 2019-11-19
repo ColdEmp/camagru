f634127e342ba3b3f313ca9d67454a67ff758b78
hbarnardWTC <wingarkorin@gmail.com>
2019-11-19 16:36:58 +0200

    Cleaning up index

--- index.php
+++ index.php
@@ -10,6 +10,15 @@
 	<div class="Level has-background-grey-dark has-text-centered">
 		<div style = "display: inline;">
 			<h1 class="title is-1 has-text-light">CAMAGRU</h1>
+		<?php
+		if (isset($_Session["user"])){
+			echo '<a href = "./login.php" class = "button is-outlined hbutton">Home</a>';
+		}
+		else
+		{
+			echo '<a href = "./pages/login.php" class = "button is-outlined logbutton">Home</a>';
+		}
+		?>
 		</div>
 			 <p class="subtitle has-text-light">A social media app!</p>
 	</div>
