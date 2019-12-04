f361361f36aa581c31cab7266d4fdfb053f7e410
hbarnardWTC <wingarkorin@gmail.com>
2019-12-04 11:51:24 +0200

    camera testing

--- pages/editor.php
+++ pages/editor.php
@@ -35,27 +35,25 @@
 				</div>
 				<h1 id = "lil" class="title is-1 has-text-light">CAMAGRU</h1>
 			</div>
-			<p id = wanka class="subtitle has-text-light">A social media app!</p>
+			<p class="subtitle has-text-light">A social media app!</p>
 		</div>
 		<section class="section is-centered">
 			<div class="columns">
 				<div class="column"></div>
 				<div class="column">
-					<div class="webcam">
+				<div class="webcam">
 						<video id = "video" playsinline autoplay></video>
 					</div>
-					<div style = "padding : 0px 0px 20px 0px">
-					<canvas id="myCanvas" width="200" height="200"></canvas>
+					<br />
+					<div class="canvas_photo">
+						<canvas id="canvas" width="640" height="480"></canvas>
 					</div>
+					<p>Webcam use</p>
+					<button id="snap" class="btn">Capture</button>
 				</div>
 				<div class="column"></div>
 			</div>
 		</section>
-		<script>
-			const video = document.getElementById('video');
-
-			video.srcObject = stream;
-		</script>
 		<footer class="credits has-background-grey">
 			<div class="content has-text-centered has-text-light">
 				<p>Camagru by <a class ="has-text-light is-italic" href="https://github.com/cameronglanville">Cameron Glanville</a>,
@@ -64,4 +62,85 @@
 			</div>
 		</footer>
 	</body>
+	<script>
+	function openSlideMenu() {
+			document.getElementById('side-menu').style.width = '250px';
+		}
+		function closeSlideMenu() {
+			document.getElementById('side-menu').style.width = '0';
+		}
+		'user strict';
+		const video = document.getElementById('video');
+		const canvas = document.getElementById('canvas');
+		const snap = document.getElementById('snap');
+		const btnDisplay = document.getElementById('btnDisplay');
+		const btnDownload = document.getElementById('btnDownload');
+		const errorMsgElement = document.getElementById('span#ErrorMsg');
+		var capture = 0;
+		const constraints = {
+			// audio: true,
+			video: {
+				width: 640, height: 480
+			}
+		};
+		// Access webcam
+		async function init() {
+			try {
+				const stream = await navigator.mediaDevices.getUserMedia(constraints);
+				handleSuccess(stream);
+			}
+			catch(e){
+				errorMsgElement.innerHTML = `navigator.getUserMedia.error:${e.toString()}`;
+				alert('Something is wrong');
+			}
+		}
+		// Success
+		function handleSuccess(stream){
+			window.stream = stream;
+			video.srcObject = stream;
+		}
+		// Load init
+		init();
+		// Draw image
+		var context = canvas.getContext('2d');
+			width = 640;
+			height = 480;
+		var img = new Image;
+		var	s_canvas;
+		snap.addEventListener("click",function(){
+			capture = 1;
+			context.drawImage(video, 0, 0, width, height);
+		});
+
+		btnDisplay.addEventListener("click", function () {
+			const dataURI = canvas.toDataURL('image/jpeg', 1.0);
+			console.log(dataURI);
+		});
+		// navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+		// 	if(navigator.getUserMedia){
+		// 		navigator.getUserMedia({video:true},handleVideo,videoError);
+		// 	}
+
+		// 	function handleVideo(stream){
+		// 	document.querySelector('#video').src = window.URL.createObjectURL(stream);
+		// 	}
+
+		// 	function videoError(e){
+		// 		alert('Something is wrong');
+		// 	}
+			// const camvideo = document.getElementById('camvideo');
+			// const constraints = 
+			// {
+			// 	video:
+			// 	{
+			// 		minAspectRatio: 1.333,
+			// 		minFrameRate: 60,
+			// 		width: 640,
+			// 		heigth: 480
+			// 	}
+			// };
+			// const stream = await navigator.mediaDevices.getUserMedia(constraints);	
+			// window.stream = stream;
+			// camvideo.srcObject = stream;
+		</script>
 </html>
