9cbfba0b4f5c27a8f550c3fc670eadb3abebe483
CaptainRedBear <twebb@student.wethinkcode.co.za>
2019-12-17 13:59:59 +0200

    image functions update, incomplete, index.php update, can't remember what, index.css update, adding ACTUAL CSS

--- config/image_functions.php
+++ config/image_functions.php
@@ -1,50 +1,156 @@
 <?PHP
+include_once '../style/index.css';
+include_once 'functions.php';
 
-function pager_images()
+function pager_images($no, $page)
 {
     try
     {
-
+        echo "<div class=\"column middle c\" onload=\"scrolltest();\">";
+        home_img($no, $page, "column middle image");
+        echo "</div>";
     }
     catch(PDOException $e)
+    {
+        echo "Failed to print home images\n";
+    }
+}
+
+function home_img($amm, $page_no, $class)
+{
+    try
+    {
+        $index = $page_no;
+        $arr = id_arr();
+        $counter = 0;
+        $max = count($arr);
+        $max -= $amm * ($page_no - 1);
+        $image = 0;
+                // Line below needs it's function to be made
+        // $posts = get_posts($page_no);
+        while ($image < $amm)
+        {
+            $i = $arr[$max - 1];
+            $page = $_GET['page'];
+            if (ver_img($i) == 0)
+            {
+                return (0);
+            }
+            else
+            {
+                $img = retrieve_img($i, $class);
+                        // Line below needs to be helped
+                // $likes = get_likes(NULL, $i);
+                
+            }
+        }
+    }
+    catch (PDOException $e)
     {
         
     }
 }
 
-function home_img()
-{
-    
-}
-
 function get_posts()
 {
-    
+    try
+    {
+
+    }
+    catch (PDOException $e)
+    {
+        
+    }
 }
 
-function retrieve_img()
+function retrieve_img($i, $class)
 {
-    
+    try
+    {
+        if(ver_img($i) == 0)
+        {
+            return (0);
+        }
+        else
+        {
+            return get_img($i, $class);
+        }
+    }
+    catch (PDOException $e)
+    {
+        echo "Failed to retrieve image\n";
+    }
 }
 
-function ver_img()
+function ver_img($imagesid)
 {
-    
+    try
+    {
+        $connection = open_connection();
+        $statement = $connection->prepare("SELECT userid FROM images WHERE imageid = '$imagesid'");
+        $statement->execute();
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+        if (!$result['userid'])
+        {
+            return (0);
+        }
+        // The line below is uncertain, re-check later
+        $statement->closeCursor();
+        return ($result['userid']);
+    }
+    catch (PDOException $e)
+    {
+        echo "Failed to verify image\n";
+    }
 }
 
-function get_img()
+function get_img($imagesid, $class)
 {
-    
+    try
+    {
+        $req = find_specified("image_src", "images", "imageid", $imagesid);
+        if ($req == false)
+        {
+            return (0);
+        }
+        if ($class)
+        {
+            $return = "<img class=\"$class\" src='$req' />";
+        }
+        else
+        {
+            $return = "<img src='$req'/>";
+        }
+        return $return;
+    }
+    catch (PDOException $e)
+    {
+        echo "Failed to get image\n";
+    }
 }
 
 function get_likes()
 {
-    
+    try
+    {
+
+    }
+    catch (PDOException $e)
+    {
+        
+    }
 }
 
 function home_get_comment()
 {
-    
+    try
+    {
+
+    }
+    catch (PDOException $e)
+    {
+        
+    }
 }
 
 
@@ -69,8 +175,7 @@ function pager($mode, $amm)
 {
     try
     {
-        // $max = count(id_arr());
-        $max = 32;
+        $max = count(id_arr());
         if ($page = $_GET['page'])
         {
             if ($page > 1 && mode == -1)
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 //include_once "./pages/header.php";
 include_once './config/functions.php';
 include_once './log/page_prot.php';
