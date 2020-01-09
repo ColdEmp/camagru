bd6a2b9cfcd13d09b4404f5e6afb53550277ab1a
hbarnardWTC <wingarkorin@gmail.com>
2020-01-09 13:50:09 +0200

    Insert meaningful commit message. Like posting is fixed

--- pages/post.php
+++ pages/post.php
@@ -6,7 +6,6 @@ $image = $_POST["img"];
 $username = $_SESSION["username"];
 
 //$binary_img = base64_encode(file_get_contents($image));
-notify("It reaches post.php");
 $image = str_replace('data:image/jpeg;base64,', '', $image);
 $image = str_replace(' ', '+', $image);
 $worthless = base64_decode($image);
