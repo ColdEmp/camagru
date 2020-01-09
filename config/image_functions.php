a6d9fbac180e5eb39bac4942bc9584d36fa352f9
Timothy Webb <twebb@c2r6s2.wethinkcode.co.za>
2020-01-09 14:11:01 +0200

    Updated image_functions and planning

--- config/image_functions.php
+++ config/image_functions.php
@@ -44,13 +44,27 @@ function home_img($amm, $page_no, $class)
                 echo ""; // for the actual display, very important, no touchie
                 // You can insert something here (inside an if statement, with it's own echo) to add a delete thingy for users that are logged in
                 echo ""; // for the comment box
-                
+               
+                if (!isset($_SESSION["username"]))
+                {
+                    // pc and them put a home_get_comment function here and used a parameter for when no one was logged in
+                }
+                else
+                {
+                    // pc and them declared a user variable and used that as a parameter for a home_get_comment function
+                }
+
+                // pc and them echoed ---> "<br/><a class=\"c-btn-close\" onclick=\"openCloseComment_$i()\">&times;</a><br/>"
             }
+            // check planning for how to do comments only if logged in
         }
+        echo "</div>";
+        $image++;
+        $max--;
     }
     catch (PDOException $e)
     {
-        
+        echo "Failed to print home page image list\n";
     }
 }
 
