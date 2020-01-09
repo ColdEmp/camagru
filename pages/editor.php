2213e8c98c470cccc0b0af2e813836fe24b6c12f
hbarnardWTC <wingarkorin@gmail.com>
2020-01-09 07:50:26 +0200

    Wah

--- pages/editor.php
+++ pages/editor.php
@@ -127,19 +127,27 @@ user_nlogged_redirect();
 		btnDisplay.addEventListener("click", function () {
 			const dataURI = canvas.toDataURL('image/jpeg', 1.0);
 		});
-		
+
 		function XHR()
 		{
-			if (capture == 1){
-				img = canvas.toDataURL('image/jpeg', 1.0);
-				// console.log(img);
-				var xhre = new XMLHttpRequest();
-				xhre.open("POST", "../log/post.php");
-				xhre.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-				var str = "action=upload&sub_action=canvas&img=" + img;
-				xhre.send(str);
+			if (capture == 1)
+			{
+				<?php ?>
 			}
 		}
+
+		// function XHR()
+		// {
+		// 	if (capture == 1){
+		// 		img = canvas.toDataURL('image/jpeg', 1.0);
+		// 		// console.log(img);
+		// 		var xhre = new XMLHttpRequest();
+		// 		xhre.open("POST", "../log/post.php");
+		// 		xhre.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		// 		var str = "action=upload&sub_action=canvas&img=" + img;
+		// 		xhre.send(str);
+		// 	}
+		// }
 
 		function circular_dragon() {
 					if (capture == 1){
