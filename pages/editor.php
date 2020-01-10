5ff364093ebd8159e2ad6276d734189ef594dcde
hbarnardWTC <wingarkorin@gmail.com>
2020-01-10 11:10:35 +0200

    Protections/resize

--- pages/editor.php
+++ pages/editor.php
@@ -58,9 +58,24 @@ user_nlogged_redirect();
 							<canvas id="canvas" width="640" height="480"></canvas>
 						</div>
 						<p>Webcam use</p>
-						<button id="snap" class="btn">Capture</button>
+						<button id="snap" class="button">Capture</button>
 						<!-- <button onclick = "postimg()" id="postbtn" class="btn">Post</button> -->
-						<button onclick="XHR()" id="btnDisplay" class="btn" >Post</button>
+						<button onclick="XHR()" id="btnDisplay" class="button" >Post</button>
+						<form method="post" enctype="multipart/form-data">
+						<div class="file">
+							<label class="file-label">
+								<input class="file-input" type="file" name="resume">
+								<span class="file-cta">
+								<span class="file-icon">
+									<i class="fa fa-upload"></i>
+								</span>
+								<span class="file-label">
+									Choose a file…
+								</span>
+								</span>
+							</label>
+						</div>
+						</form>
 					</div>
 					<!-- Rightside Column -->
 					<div class="column is-one-quarter">
